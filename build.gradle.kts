17f28122f3a789e724d985d8c1f86ed62077cfdd
Votond <votond@outlook.com>
2025-05-27 10:57:05 +0300

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.1.21"
-    kotlin("plugin.serialization") version "2.1.21"
     id("com.gradleup.shadow") version "8.3.6"
     id("de.eldoria.plugin-yml.bukkit") version "0.7.1"
 }
